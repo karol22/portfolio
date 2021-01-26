0121fa437d657df7bb61391982f5a969d1e4bd5b
Karol Gutiérrez <kjgs.22@gmail.com>
2021-01-25 19:17:44 -0600

    59. Add integrated index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
+# Portfolio controller
+# This comment is to comply with rubocop
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
 end
