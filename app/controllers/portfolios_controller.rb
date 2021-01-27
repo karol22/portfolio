63307b6e1e8d8107ed7a2bc296cf77774afc5de8
Karol Gutiérrez <kjgs.22@gmail.com>
2021-01-26 21:56:49 -0600

    61. Add edit action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -23,6 +23,21 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: "Record was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def portfolio_params
