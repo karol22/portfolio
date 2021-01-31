aaf03ef360e402ae6595bc7f0dcf962acda3c1b1
Karol Gutiérrez <kjgs.22@gmail.com>
2021-01-30 22:44:54 -0600

    Fix bug missing class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,23 @@
-# frozen_string_literal: true
-
-# Helper for html
+# General helper
 module ApplicationHelper
-  def login_helper
-    if current_user.is_a?(User)
-      link_to 'Logout', destroy_user_session_path, method: :delete
+  def login_helper(style = '')
+    if current_user.is_a?(GuestUser)
+      (link_to 'Register', new_user_registration_path, class: style) +
+        ' '.html_safe +
+        (link_to 'Login', new_user_session_path, class: style)
     else
-      (link_to 'Register', new_user_registration_path) +
-        '<br>'.html_safe +
-        (link_to 'Login', new_user_session_path)
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     end
   end
 
   def source_helper(layout_name)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-      content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, and you are on the #{layout_name} layout."
+      content_tag(:p, greeting.html_safe, class: 'source-greeting')
     end
   end
 
   def copyright_generator
     DevcampViewToolK::Renderer.copyright 'Karol Gutiérrez', 'All rights reserved'
   end
-
 end
-
